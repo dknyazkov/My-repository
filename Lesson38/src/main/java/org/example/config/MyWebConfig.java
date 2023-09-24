9d5f12325c38456d40ad47736a6e939e5b263889
dknyazkov <129373189+dknyazkov@users.noreply.github.com>
2023-09-24 12:49:17 +0300

    Lesson39

--- Lesson38/src/main/java/org/example/config/MyWebConfig.java
+++ Lesson38/src/main/java/org/example/config/MyWebConfig.java
@@ -1,18 +1,42 @@
 package org.example.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class MyWebConfig {
     @Bean
-    public ViewResolver viewResolver(){
-        InternalResourceViewResolver  internalResourceViewResolver=new InternalResourceViewResolver();
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource=new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.of("ru","RUS"));
+        return localeResolver;
+
+
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
         internalResourceViewResolver.setPrefix("/WEB-INF/");
         internalResourceViewResolver.setSuffix(".jsp");
-       return internalResourceViewResolver;
+        return internalResourceViewResolver;
     }
 }
